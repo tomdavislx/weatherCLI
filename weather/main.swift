f64d16531cd040e04a6e22f97481ccdac792343e
Tom Davis <tom@tomdavislx.co.uk>
2017-01-16 11:30:27 +0000

    Initial Commit

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by Tom Davis on 16/01/2017.
+//  Copyright © 2017 Tom Davis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
